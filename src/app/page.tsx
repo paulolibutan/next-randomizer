808e2350ae4846099652186c7df2727f5066d031
Paulo Libutan <libutanpaulo@gmail.com>
2024-12-07 02:10:17 +0000

    Remove unsupported code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -190,7 +190,7 @@ const Home: React.FC = () => {
 
   const titleCase = (str: any) => {
     str = str.toLowerCase().split(" ");
-    for (var i = 0; i < str.length; i++) {
+    for (let i = 0; i < str.length; i++) {
       str[i] = str[i].charAt(0).toUpperCase() + str[i].slice(1);
     }
     return str.join(" ");
